2266bf7b67c0ed8e21d3d16a5d9219a7a75a8547
ARPITSHAH\arpit <arpit.shah1@gmail.com>
2018-04-29 13:39:28 +1200

    Cosmetic changes

--- src/tools/generateTestPlan/Test_generate_test_plan.java
+++ src/tools/generateTestPlan/Test_generate_test_plan.java
@@ -5,16 +5,7 @@ import com.arpitos.infra.annotation.GenerateTestPlan;
 import com.arpitos.infra.annotation.Testcase;
 import com.arpitos.interfaces.TestExecutable;
 
-
-// @formatter:off
-@Testcase(skip=false, scenario = "Test_generate_test_plan",
-decription = "\nGIVEN : "
-           + "\nAND   : "
-           + "\nWHEN  : "
-           + "\nAND   : "
-           + "\nTHEN  : "
-, preparedBy = "arpit", preparationDate = "8/04/2018", reviewedBy = "", reviewDate = "")
-// @formatter:on
+@Testcase(skip = false, scenario = "Test_generate_test_plan", decription = "", preparedBy = "arpit", preparationDate = "29/04/2018", reviewedBy = "", reviewDate = "")
 public class Test_generate_test_plan implements TestExecutable {
 
 	public void onExecute(TestContext context) throws Exception {
@@ -23,10 +14,10 @@ public class Test_generate_test_plan implements TestExecutable {
 
 	public void execute(TestContext context) throws Exception {
 
-		context.setKnownToFail(false, "");
-		//--------------------------------------------------------------------------------------------
-		GenerateTestPlan testPlan = new GenerateTestPlan(context, "unit_test");
-		//--------------------------------------------------------------------------------------------
+		context.setKnownToFail(false, "Ticket-????");
+		// --------------------------------------------------------------------------------------------
+		GenerateTestPlan testPlan = new GenerateTestPlan(context, "unit_test.Covert");
+		// --------------------------------------------------------------------------------------------
 
 	}
 }
